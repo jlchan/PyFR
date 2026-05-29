83fcc01f2a4494b2466d1aeb50c980181cfb40b3
Jesse Chan <jesse.lee.chan@gmail.com>
2026-05-29 15:41:11 -0500

    print out option

--- pyfr/solvers/navstokes/system.py
+++ pyfr/solvers/navstokes/system.py
@@ -1,3 +1,4 @@
+from pyfr.mpiutil import get_comm_rank_root
 from pyfr.solvers.baseadvecdiff import BaseAdvectionDiffusionSystem
 from pyfr.solvers.navstokes.elements import NavierStokesElements
 from pyfr.solvers.navstokes.inters import (NavierStokesBaseBCInters,
@@ -13,3 +14,13 @@ class NavierStokesSystem(BaseAdvectionDiffusionSystem):
     intinterscls = NavierStokesIntInters
     mpiinterscls = NavierStokesMPIInters
     bbcinterscls = NavierStokesBaseBCInters
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        comm, rank, root = get_comm_rank_root()
+        if rank == root:
+            gradvars = self.cfg.get(
+                'solver', 'gradient-variables', 'conservative'
+            )
+            print(f'Running with gradient-variables = {gradvars}')
